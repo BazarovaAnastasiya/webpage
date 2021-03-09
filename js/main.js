18de3ebd2943a2a3cbfa1aca06ae3da4101fa03e
Bazarova Anastasiya <nassi_13@inbox.ru>
2021-03-09 16:45:38 +0300

    modified:css/main.css

--- js/main.js
+++ js/main.js
@@ -3,11 +3,25 @@
 $(function(){
 	
   $('.header__burger').on('click',function() {
-  	$('.header__navbar').slideToggle();
     $('.burger').toggleClass('active');
-    $('.header__navbar').toggleClass('flex');
-  });
- 
 
+    $('.header__navbar').slideToggle(200, "linear",function(){
+      if($(this).css('display') === "none"){
+        $(this).removeAttr('style');
+      }
+      if($(this).css('display') === "block"){
+      $(this).css('display', 'flex');
+      }
+      });
+
+  });
 
 });
+
+// const bth = document.querySelector('.header__burger')
+// const navbar = document.querySelector('.header__navbar')
+// console.log(bth);
+
+// bth.onclick = function(){
+//   navbar.classList.toggle("active")
+// }
